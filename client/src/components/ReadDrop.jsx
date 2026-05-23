baccc0277862bf65567f5f32ff6c6839c80d53dd
adityaexe <kmradi2002@gmail.com>
2026-05-23 12:24:30 +0530

    refactor: remove unnecessary React import in ReadDrop component

--- client/src/components/ReadDrop.jsx
+++ client/src/components/ReadDrop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import { importKey, decryptMessage, deriveKeyFromPassword } from '../utils/crypto';
 
